fb0c9ba55247bf901e4e3978c5f52013ba81f9c0
Sai Kishore Veeranki <saikishoreveeranki@gmail.com>
2026-09-24 12:40:16 +0530

    test: devtrust push testing

---
+++ src/main/java/com/vsk/devtrust_demo_client/controller/DemoEndPoint.java
@@ -0,0 +1,14 @@
+package com.vsk.devtrust_demo_client.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/demo")
+public class DemoEndPoint {
+
+    @GetMapping
+    public String getName() {
+        return "Hello from DevTrust Demo Client!";
+    }
+}
